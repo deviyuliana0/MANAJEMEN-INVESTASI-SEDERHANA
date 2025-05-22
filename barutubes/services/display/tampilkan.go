7a73761da1e35abfa2a29063c4ebeabc40d1965b
deviyuliana0 <depiana1007@gmail.com>
2025-05-23 00:57:39 +0700

    Create tampilkan.go

---
+++ barutubes/services/display/tampilkan.go
@@ -0,0 +1,9 @@
+package display
+
+import (
+	"invest_proj/models"
+)
+
+func tampilkanInvestasi() { // fungsi untuk menampilkan data investasi
+	models.TampilkanInvestasi() // memanggil fungsi TampilkanInvestasi() dari models
+}
