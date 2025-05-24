8c8b5d42585b6ea3f2fe64a24ca850573a6cd5f8
deviyuliana0 <depiana1007@gmail.com>
2025-05-24 15:31:11 +0700

    Update searchnama.go

--- barutubes/services/searching/searchnama.go
+++ barutubes/services/searching/searchnama.go
@@ -5,28 +5,29 @@ import (
 	"invest_proj/models"
 )
 
-func cariBerdasarkanNama() {
-	if len(models.Portofolio) == 0 {
-		fmt.Println("Belum ada data untuk dicari")
+func CariBerdasarkanNama() {
+	if models.JumlahData == 0 {
+		fmt.Println("Belum ada data untuk dicari.")
 		return
 	}
 
 	var nama string
-	fmt.Print("Masukkan nama aset yang dicari : ")
+	fmt.Print("Masukkan nama aset yang dicari: ")
 	fmt.Scan(&nama)
 
 	ditemukan := false
-	for _, invest := range models.Portofolio {
+	for i := 0; i < models.JumlahData; i++ {
+		invest := models.Portofolio[i]
 		if invest.NamaAset == nama {
 			fmt.Println("Ditemukan:")
-			fmt.Printf("%s (%s), Beli : %.2f, Sekarang : %.2f, Unit : %d\n", invest.NamaAset, invest.JenisAset, invest.HargaBeli, invest.HargaSekarang, invest.JumlahUnit) // menampilkan data aset yang ditemukan
+			fmt.Printf("%s (%s), Beli: %.2f, Sekarang: %.2f, Unit: %d\n",
+				invest.NamaAset, invest.JenisAset, invest.HargaBeli, invest.HargaSekarang, invest.JumlahUnit)
 			ditemukan = true
 			break
 		}
 	}
 
 	if !ditemukan {
-		models.cariBerdasarkanNama()
-		fmt.Println("Data Tidak ditemukan.")
+		fmt.Println("Data tidak ditemukan.")
 	}
 }
