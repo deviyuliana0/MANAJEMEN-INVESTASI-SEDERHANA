f26502243d0ae16f8009e06c068d94b906a7720a
deviyuliana0 <depiana1007@gmail.com>
2025-05-24 15:27:11 +0700

    Update tampilkan.go

--- barutubes/services/display/tampilkan.go
+++ barutubes/services/display/tampilkan.go
@@ -1,9 +1,19 @@
 package display
 
 import (
+	"fmt"
 	"invest_proj/models"
 )
 
-func tampilkanInvestasi() { // fungsi untuk menampilkan data investasi
-	models.TampilkanInvestasi() // memanggil fungsi TampilkanInvestasi() dari models
+func TampilkanInvestasi() {
+	fmt.Println("=== Daftar Investasi ===")
+	for i := 0; i < models.JumlahData; i++ {
+		fmt.Printf("[%d] %s (%s), Beli: %.2f, Sekarang: %.2f, Unit: %d\n",
+			i+1,
+			models.Portofolio[i].NamaAset,
+			models.Portofolio[i].JenisAset,
+			models.Portofolio[i].HargaBeli,
+			models.Portofolio[i].HargaSekarang,
+			models.Portofolio[i].JumlahUnit)
+	}
 }
