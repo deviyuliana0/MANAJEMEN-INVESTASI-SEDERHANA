3687e4d6d71eec0b07cf5be44b1e3f2784ffa4b1
deviyuliana0 <depiana1007@gmail.com>
2025-05-24 15:27:22 +0700

    Update hapus.go

--- barutubes/services/operations/hapus.go
+++ barutubes/services/operations/hapus.go
@@ -5,23 +5,28 @@ import (
 	"invest_proj/models"
 )
 
-func hapusInvestasi() {
-	if len(models.Portofolio) == 0 { // cek apakah portofolio kosong
-		fmt.Println("Belum ada data untuk dihapus")
+func HapusInvestasi() {
+	if models.JumlahData == 0 {
+		fmt.Println("Belum ada data untuk dihapus.")
 		return
 	}
 
-	var index int                                                // variabel untuk menampung index investasi yang ingin dihapus
-	fmt.Print("Pilih nomor data Investasi yang ingin dihapus: ") // menampilkan pesan
+	var index int
+	fmt.Printf("Pilih nomor data Investasi yang ingin dihapus (1 - %d): ", models.JumlahData)
 	fmt.Scan(&index)
 
-	if index < 1 || index > len(models.Portofolio) { // cek apakah index valid
-		fmt.Println("Index tidak valid")
+	if index < 1 || index > models.JumlahData {
+		fmt.Println("Index tidak valid.")
 		return
 	}
 
-	i := index - 1                                                                // mengubah index menjadi indeks array
-	models.Portofolio = append(models.Portofolio[:i], models.Portofolio[i+1:]...) // menghapus data investasi
-	models.hapusInvest()
-	fmt.Print("Data berhasil dihapus!\n\n")
+	i := index - 1
+
+	// Geser elemen setelah i ke kiri
+	for j := i; j < models.JumlahData-1; j++ {
+		models.Portofolio[j] = models.Portofolio[j+1]
+	}
+
+	models.JumlahData-- // kurangi jumlah data aktif
+	fmt.Println("Data berhasil dihapus!\n")
 }
