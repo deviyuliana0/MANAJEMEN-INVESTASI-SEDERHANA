448f150d2ee1beb18d7763f3bc5b3755aaf3d80a
deviyuliana0 <depiana1007@gmail.com>
2025-05-24 15:27:01 +0700

    Update investasi.go

--- barutubes/models/investasi.go
+++ barutubes/models/investasi.go
@@ -1,12 +1,14 @@
 package models
 
-type Investasi struct { // struct untuk menampung data investasi
-	NamaAset      string  // nama aset yang diinvestasikan
-	JenisAset     string  // jenis aset yang diinvestasikan
-	HargaBeli     float64 // harga beli aset per unit
-	HargaSekarang float64 // harga aset per unit saat ini
-	JumlahUnit    int     // jumlah unit aset yang diinvestasikan
+type Investasi struct {
+	NamaAset      string
+	JenisAset     string
+	HargaBeli     float64
+	HargaSekarang float64
+	JumlahUnit    int
 }
 
-var Portofolio []Investasi // array untuk menampung data portofolio
-var DaftarTransaksi []Investasi
+const MaksData = 100
+
+var Portofolio [MaksData]Investasi // ✅ huruf besar → public/exported
+var JumlahData int                 // ✅ agar bisa dipakai di package lain
