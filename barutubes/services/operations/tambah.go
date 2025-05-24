dea6db5cf93b40742d13c747810e1ccb3701f1ba
deviyuliana0 <depiana1007@gmail.com>
2025-05-24 15:30:35 +0700

    Update tambah.go

--- barutubes/services/operations/tambah.go
+++ barutubes/services/operations/tambah.go
@@ -5,25 +5,30 @@ import (
 	"invest_proj/models"
 )
 
-func TambahInvestasi() { // fungsi untuk menambahkan investasi ke portofolio
-	var invest models.Investasi // variabel untuk menampung data investasi yang akan ditambahkan
+func TambahInvestasi() {
+	var invest models.Investasi
 
 	fmt.Println("=== Tambah Data Investasi ===")
-	fmt.Print("Nama Aset	:")   // menampilkan input nama aset
-	fmt.Scan(&invest.NamaAset) // menginput nama aset
+	fmt.Print("Nama Aset       : ")
+	fmt.Scan(&invest.NamaAset)
 
-	fmt.Print("Jenis Aset	:")   // jenis aset yang diinvestasikan
-	fmt.Scan(&invest.JenisAset) // jenis aset yang diinvestasikan
+	fmt.Print("Jenis Aset      : ")
+	fmt.Scan(&invest.JenisAset)
 
-	fmt.Print("Harga Beli	:")   // harga beli aset per unit
-	fmt.Scan(&invest.HargaBeli) // input harga beli aset per unit
+	fmt.Print("Harga Beli      : ")
+	fmt.Scan(&invest.HargaBeli)
 
-	fmt.Print("Harga Sekarang	:")   // harga aset per unit saat ini
-	fmt.Scan(&invest.HargaSekarang) // input harga aset saat ini
+	fmt.Print("Harga Sekarang  : ")
+	fmt.Scan(&invest.HargaSekarang)
 
-	fmt.Print("Jumlah Unit	:")   // jumlah unit aset yang diinvestasikan
-	fmt.Scan(&invest.JumlahUnit) // input jumlah unit aset yang diinvestasikan
+	fmt.Print("Jumlah Unit     : ")
+	fmt.Scan(&invest.JumlahUnit)
 
-	models.Portofolio = append(models.Portofolio, invest) // menambahkan data investasi ke array portofolio
-	fmt.Print("Data berhasil diinputkan!\n\n")            // menampilkan pesan berhasil input data
+	if models.JumlahData < models.MaksData {
+		models.Portofolio[models.JumlahData] = invest
+		models.JumlahData++
+		fmt.Println("Data berhasil diinputkan!\n")
+	} else {
+		fmt.Println("Data gagal ditambahkan: kapasitas maksimum tercapai.")
+	}
 }
