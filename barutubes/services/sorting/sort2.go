87076602e6386ff5d6685a2e1a177ae0b51e761d
deviyuliana0 <depiana1007@gmail.com>
2025-05-24 15:31:18 +0700

    Update sort2.go

--- barutubes/services/sorting/sort2.go
+++ barutubes/services/sorting/sort2.go
@@ -2,20 +2,25 @@ package sorting
 
 import (
 	"fmt"
-	"proj_invest/models"
+	"invest_proj/models"
 )
 
-func urutkanReturn() { // insertion sort
-	for i := 1; i < len(models.Portofolio); i++ {
+// Fungsi bantu untuk menghitung return
+func hitungReturn(invest models.Investasi) float64 {
+	return (invest.HargaSekarang - invest.HargaBeli) * float64(invest.JumlahUnit)
+}
+
+// Insertion sort berdasarkan return tertinggi ke terendah
+func UrutkanReturn() {
+	for i := 1; i < models.JumlahData; i++ {
 		key := models.Portofolio[i]
 		j := i - 1
-		for j >= 0 && hitungReturn(models.Portofolio[j]) > hitungReturn(key) {
+
+		for j >= 0 && hitungReturn(models.Portofolio[j]) < hitungReturn(key) {
 			models.Portofolio[j+1] = models.Portofolio[j]
 			j--
 		}
 		models.Portofolio[j+1] = key
 	}
-
-	models.urutkanReturn()
-	fmt.Println("Data berhasil diurutkan berdasarkan return investasi.") // tertinggi ke rendah
+	fmt.Println("Data berhasil diurutkan berdasarkan return investasi (dari tertinggi ke terendah).")
 }
