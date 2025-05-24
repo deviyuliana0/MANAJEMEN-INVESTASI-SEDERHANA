87f18aeef2e34e35e6aaeae4a005048f30dd47b7
deviyuliana0 <depiana1007@gmail.com>
2025-05-24 15:31:06 +0700

    Update searchjenis.go

--- barutubes/services/searching/searchjenis.go
+++ barutubes/services/searching/searchjenis.go
@@ -5,27 +5,28 @@ import (
 	"invest_proj/models"
 )
 
-func cariBerdasarkanJenis() {
-	if len(models.Portofolio) == 0 {
-		fmt.Println("Belum ada data untuk dicari")
+func CariBerdasarkanJenis() {
+	if models.JumlahData == 0 {
+		fmt.Println("Belum ada data untuk dicari.")
 		return
 	}
 
 	var jenis string
-	fmt.Print("Masukkan jenis aset yang dicari (saham, obligasi, emas, reksana: ") // menampilkan pesan
+	fmt.Print("Masukkan jenis aset yang dicari (saham, obligasi, emas, reksadana): ")
 	fmt.Scan(&jenis)
 
 	ditemukan := false
 	fmt.Println("=== Hasil Pencarian ===")
-	for _, invest := range models.Portofolio {
+	for i := 0; i < models.JumlahData; i++ {
+		invest := models.Portofolio[i]
 		if invest.JenisAset == jenis {
-			fmt.Printf("%s (%s), Beli : %.2f, Sekarang : %.2f, Unit : %d\n", invest.NamaAset, invest.JenisAset, invest.HargaBeli, invest.HargaSekarang, invest.JumlahUnit)
+			fmt.Printf("%s (%s), Beli: %.2f, Sekarang: %.2f, Unit: %d\n",
+				invest.NamaAset, invest.JenisAset, invest.HargaBeli, invest.HargaSekarang, invest.JumlahUnit)
 			ditemukan = true
 		}
 	}
 
 	if !ditemukan {
-		models.cariBerdasarkanNama()
-		fmt.Println("Data Investasi Tidak ditemukan dengan jenis tersebut.")
+		fmt.Println("Data investasi tidak ditemukan dengan jenis tersebut.")
 	}
 }
